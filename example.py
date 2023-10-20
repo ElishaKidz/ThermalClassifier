2479ef1f17cf1bb881cb33fa3470809398279288
eitan.b09@gmail.com <eitan.b09@gmil.com>
2023-10-20 12:55:17 +0000

    merged !

--- example.py
+++
@@ -1,4 +0,0 @@
-from google.cloud import storage
-client = storage.Client(project='mod-gcp-white-soi-dev-1')
-bucket = client.bucket('civilian-benchmark-datasets')
-print([blob for blob in bucket.list_blobs(prefix='hit-uav')])
