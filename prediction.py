818e7d1942b3ad782d53dc1c3dea898c6bfa1f28
Sharon Komissarov <sharon200102@gmail.com>
2023-10-25 13:10:45 +0000

    transfered most of the prediction code into a predictor class

---
+++ prediction.py
@@ -0,0 +1,64 @@
+from argparse import ArgumentParser
+from pathlib import Path
+import pandas as pd
+import utils
+import cv2 as cv
+import logging
+from PIL import Image
+from predictor import ThermalPredictior
+
+parser = ArgumentParser()
+parser.add_argument('--video_path',type=str)
+parser.add_argument('--video_bboxes_path', type=str)
+parser.add_argument('--ckpt_path',type = str,default='gs://soi-models/VMD-classifier/debug/checkpoints/epoch=76-step=4851.ckpt')
+parser.add_argument('--model_name',default='resnet18',type=str)
+parser.add_argument('--num_target_classes',type=int,default=4)
+
+parser.add_argument('--frame_col_name',type=str,default='frame_num')
+parser.add_argument('--bbox_col_names',nargs='+',default=['x','y','width','height'])
+parser.add_argument('--class_col_name',type=str,default='cls')
+parser.add_argument('--bbox_format',type=str,default='coco')
+parser.add_argument('--frame_limit',type=int,default=500)
+parser.add_argument('--device',type=int,default=None)
+
+parser.add_argument('--bbox_save_path',type=str,default=Path('outputs/bboxes/result.csv'))
+parser.add_argument('--rendered_video_save_path',type=str,default=Path('outputs/videos/result.mp4'))
+
+logging.basicConfig(level=logging.DEBUG)
+args = parser.parse_args()
+video_cap = utils.create_video_capture(args.video_path)
+
+# The following raw assumes that all models constructors accept only num of classes as input, not sure that this assumption will hold. 
+
+predictor = ThermalPredictior(args.model_name,args.ckpt_path,True,args.device)
+
+bboxes_df = pd.read_csv(args.video_bboxes_path,index_col=0)
+translated_predictions = []
+while True:
+        frame_num = video_cap.get(cv.CAP_PROP_POS_FRAMES)
+        logging.debug(f'frame number {frame_num} is processed')
+        success, frame = video_cap.read(0)
+        if not success or frame_num>= args.frame_limit:
+            break
+
+        frame = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
+        frame = Image.fromarray(frame)
+
+        frame_related_bboxes = bboxes_df[bboxes_df[args.frame_col_name]==frame_num][args.bbox_col_names].values
+        if len(frame_related_bboxes) == 0:
+            continue
+
+        preds = predictor.predict_frame_bboxes(frame,frame_related_bboxes,args.bbox_format) 
+        translated_predictions.extend(preds)
+        
+bboxes_with_class_predicions = bboxes_df.assign(**{args.class_col_name:translated_predictions})
+
+if args.bbox_save_path is not None:
+    bboxes_with_class_predicions.to_csv(args.bbox_save_path)
+
+if args.rendered_video_save_path is not None:
+    video_cap.set(cv.CAP_PROP_POS_FRAMES, 0)
+    utils.draw_video_from_bool_csv(video_cap,bboxes_with_class_predicions,args.bbox_col_names,args.rendered_video_save_path,
+    args.class_col_name,args.bbox_format,args.frame_limit)
+
+
--- predictor.py
+++ predictor.py
@@ -1,105 +1,57 @@
-from argparse import ArgumentParser
-from pathlib import Path
-from datasets.classes import ImageSample
-from models.resnet import ModelRepo 
-import pandas as pd
-import utils
-import cv2 as cv
-from pybboxes import BoundingBox
-import pybboxes as pbx
-from transforms.prepare_to_models import Model2Transforms
 import torch
-import logging
-from PIL import Image
-from torchvision.transforms.functional import pil_to_tensor
 import gcsfs
+from models.resnet import ModelRepo
+from SoiUtils.general import get_device
 from transforms import inference_transforms
+import pybboxes as pbx
+from pybboxes import BoundingBox
+from datasets.classes import ImageSample
+import numpy as np
+from PIL import Image
 
+class ThermalPredictior:
+    FS = gcsfs.GCSFileSystem(project="mod-gcp-white-soi-dev-1")
 
-def load_model(model_class, ckpt):
-    torch_dict = torch.load(fs.open(ckpt, "rb"), map_location='cpu')
-    state_dict = {key.replace("model.", "") : value for key, value in torch_dict["state_dict"].items()}     
-    class2idx = torch_dict['hyper_parameters']["class2idx"]
-    # TODO we need to save idx2class
-    model = model_class(len(class2idx))
-    model.load_state_dict(state_dict)
+    def __init__(self,model_name,ckpt_path,load_from_remote=True,device='cpu'):
+        self.device = get_device(device)
+    
+        if load_from_remote:
+            torch_dict = torch.load(ThermalPredictior.FS.open(ckpt_path, "rb"), map_location='cpu')
+        else:
+            torch_dict = torch.load(ckpt_path,map_location='cpu')
         
-    return model, class2idx
+        class2idx = torch_dict['hyper_parameters']["class2idx"]
+        self.classes_to_labels_translation = {index: class_name for class_name, index in class2idx.items()}
 
 
-parser = ArgumentParser()
-parser.add_argument('--video_path',type=str)
-parser.add_argument('--video_bboxes_path', type=str)
-parser.add_argument('--ckpt_path',type = str,default='gs://soi-models/VMD-classifier/debug/checkpoints/epoch=76-step=4851.ckpt')
-parser.add_argument('--model_name',default='resnet18',type=str)
-parser.add_argument('--num_target_classes',type=int,default=4)
+        state_dict = {key.replace("model.", "") : value for key, value in torch_dict["state_dict"].items()}     
+        self.model = ModelRepo.registry[model_name](len(class2idx))
+        self.model.load_state_dict(state_dict)
+        self.model.to(self.device)
+        self.model.eval()
 
-parser.add_argument('--frame_col_name',type=str,default='frame_num')
-parser.add_argument('--bbox_col_names',nargs='+',default=['x','y','width','height'])
-parser.add_argument('--class_col_name',type=str,default='cls')
-parser.add_argument('--bbox_format',type=str,default='coco')
-parser.add_argument('--frame_limit',type=int,default=500)
-parser.add_argument('--device',type=int,default=None)
-
-parser.add_argument('--bbox_save_path',type=str,default=Path('outputs/bboxes/result.csv'))
-parser.add_argument('--rendered_video_save_path',type=str,default=Path('outputs/videos/result.mp4'))
-
-logging.basicConfig(level=logging.DEBUG)
-args = parser.parse_args()
-video_cap = utils.create_video_capture(args.video_path)
-
-# The following raw assumes that all models constructors accept only num of classes as input, not sure that this assumption will hold. 
-
-fs = gcsfs.GCSFileSystem(project="mod-gcp-white-soi-dev-1")
-
-device = 'cpu' if args.device is None else torch.device(f'cuda:{args.device}')
-thermal_model, class2idx = load_model(ModelRepo.registry[args.model_name], args.ckpt_path)
-thermal_model.to(device)
-thermal_model.eval()
-classes_to_labels_translation = {index: class_name for class_name, index in class2idx.items()}
+        
+        self.transforms = inference_transforms()
 
 
-bboxes_df = pd.read_csv(args.video_bboxes_path,index_col=0)
-
-transfrom = inference_transforms() #Model2Transforms.registry[args.model_name]()
-predictions = []
-while True:
-        frame_num = video_cap.get(cv.CAP_PROP_POS_FRAMES)
-        logging.debug(f'frame number {frame_num} is processed')
-        success, frame = video_cap.read(0)
-        if not success or frame_num>= args.frame_limit:
-            break
-
-        frame = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
-        frame = Image.fromarray(frame)
-
-        frame_related_bboxes = bboxes_df[bboxes_df[args.frame_col_name]==frame_num][args.bbox_col_names].values
-        if len(frame_related_bboxes) == 0:
-            continue
+    def predict_frame_bboxes(self,frame:Image,frame_related_bboxes:np.array,bboxes_format:str='coco'):
 
         frame_crops_according_to_bboxes = []
         for i in range(frame_related_bboxes.shape[0]):
             frame_related_bbox = frame_related_bboxes[i,:]
-            frame_related_bbox = BoundingBox.from_coco(*pbx.convert_bbox(frame_related_bbox,from_type=args.bbox_format,to_type='coco'))
+            frame_related_bbox = BoundingBox.from_coco(*pbx.convert_bbox(frame_related_bbox,from_type=bboxes_format,to_type='coco'))
             x0, y0, x1, y1 = frame_related_bbox.to_voc().raw_values
             #croped_frame = frame[:, y0: y1, x0: x1].float().div(255.0)
             sample = ImageSample(image=frame, label=None)
             sample.metadata = {'crop_coordinates': [x0, y0, x1, y1]}
             frame_crops_according_to_bboxes.append(sample)
         
-        crops_ready_to_model = list(map(lambda sample: transfrom(sample).image, frame_crops_according_to_bboxes))
-        batch = torch.stack(crops_ready_to_model,axis=0).to(device)
-        preds = torch.argmax(thermal_model(batch), dim=1)
-        predictions.extend(list(preds.cpu().detach().tolist()))
+        crops_ready_to_model = list(map(lambda sample: self.transforms(sample).image, frame_crops_according_to_bboxes))
+        batch = torch.stack(crops_ready_to_model,axis=0).to(self.device)
+        preds = torch.argmax(self.model(batch), dim=1).cpu().detach().tolist()
+        translated_preds = list(map(lambda x: self.classes_to_labels_translation[x],preds))
         
-predictions_to_class_str = list(map(lambda x: classes_to_labels_translation[x],predictions))
-bboxes_with_class_predicion = bboxes_df.assign(**{args.class_col_name:predictions_to_class_str})
-if args.bbox_save_path is not None:
-    bboxes_with_class_predicion.to_csv(args.bbox_save_path)
-
-if args.rendered_video_save_path is not None:
-    video_cap.set(cv.CAP_PROP_POS_FRAMES, 0)
-    utils.draw_video_from_bool_csv(video_cap,bboxes_with_class_predicion,args.bbox_col_names,args.rendered_video_save_path,
-    args.class_col_name,args.bbox_format,args.frame_limit)
+        
+        return translated_preds
 
 
