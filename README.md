2c56d7e10509b23bb0c5d96a8624acd1d323f140
eitan.b09@gmail.com <eitan.b09@gmil.com>
2023-11-27 20:44:55 +0000

    code configurable now

--- README.md
+++ README.md
@@ -1,6 +1,2 @@
 # ThermalClassifier
 Creating a Bbox classifier 
-
-# Setup
-To run this code you need to set an env varible named: root_data_dir
-root_data_dir - where all your data placed.
--- main.py
+++ main.py
@@ -5,26 +5,49 @@ from lightning.pytorch.loggers import WandbLogger
 from ThermalClassifier.image_multiclass_trainer import BboxMultiClassClassifier
 import os
 from SoiUtils.load import load_yaml
+import argparse
+import json
 
-args = load_yaml('configs/thermal.yaml')
-args['root_data_dir'] = os.environ['root_data_dir']
-new_class2index = {name.lower(): i for i, name in enumerate(args['classes'])}
+def update_cfg(data, updates):
+    for key, value in updates.items():
+        data[key] = value
 
-if args['add_background_label']:
-    new_class2index['BACKGROUND'] = len(args['classes'])
-    args['classes'].append('BACKGROUND')
+parser = argparse.ArgumentParser()
+parser.add_argument('--config_path', type=str, required=True, help='YAML path')
+parser.add_argument('--root_data_dir', type=str, required=True, help='root data dir')
+parser.add_argument('--updates', type=str, help='JSON-like string of key-value pairs for updates \
+                    (e.g., {"epochs": 10, "learning_rate": 0.001, "labels": ["label1", "label2", "label3"]})')
+args = parser.parse_cfg()
+
+cfg = load_yaml(args.config_path)
+cfg['root_data_dir'] = args.root_data_dir
+
+if args.updates:
+    try:
+        updates = json.loads(args.updates)
+        update_cfg(cfg, updates)
+        print(f'Updated parameters in {args.file_path}')
+    except json.JSONDecodeError:
+            print('Invalid JSON-like syntax. Please provide updates in the correct format.')
+
+
+new_class2index = {name.lower(): i for i, name in enumerate(cfg['classes'])}
+
+if cfg['add_background_label']:
+    new_class2index['BACKGROUND'] = len(cfg['classes'])
+    cfg['classes'].append('BACKGROUND')
 ###
 
-model = BboxMultiClassClassifier(class2idx=new_class2index, model_name=args['model'])
+model = BboxMultiClassClassifier(class2idx=new_class2index, model_name=cfg['model'])
 
-data_module = GenericDataModule(root_dir=args['root_data_dir'],
-                                train_datasets_names=args['train_datasets'],
-                                val_datasets_names=args['val_datasets'],
-                                test_datasets_names=args['test_datasets'],
+data_module = GenericDataModule(root_dir=cfg['root_data_dir'],
+                                train_datasets_names=cfg['train_datasets'],
+                                val_datasets_names=cfg['val_datasets'],
+                                test_datasets_names=cfg['test_datasets'],
                                 class2idx=new_class2index,
                                 model_transforms=model.get_model_transforms())
 
-checkpoint_callback = ModelCheckpoint(dirpath=f"gcs://soi-models/VMD-classifier/{args['exp_name']}/checkpoints",
+checkpoint_callback = ModelCheckpoint(dirpath=f"gcs://soi-models/VMD-classifier/{cfg['exp_name']}/checkpoints",
                                       monitor='val_MulticlassAccuracy',
                                       mode='max',
                                       verbose=True)
@@ -33,14 +56,14 @@ callbacks = [checkpoint_callback]
 wandb_logger = WandbLogger(project="VMD-classifier")
 
 
-trainer = pl.Trainer(default_root_dir=f"gcs://soi-models/VMD-classifier/{args['exp_name']}",
+trainer = pl.Trainer(default_root_dir=f"gcs://soi-models/VMD-classifier/{cfg['exp_name']}",
                     accelerator='gpu',
-                    devices=args['devices'],
+                    devices=cfg['devices'],
                     callbacks=callbacks,
                     logger=wandb_logger,
-                    max_epochs=args['epochs'])
+                    max_epochs=cfg['epochs'])
 
 trainer.fit(model, datamodule=data_module)
 
-if args['test_datasets'] != []:
+if cfg['test_datasets'] != []:
     trainer.test(model, datamodule=data_module, ckpt_path='best')
